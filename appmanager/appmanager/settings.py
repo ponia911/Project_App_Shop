5ab89455c6afdf6736475235d014b455912c93bb
Andrei <poniaable@gmail.com>
2023-10-17 22:22:24 +0300

    master

--- appmanager/appmanager/settings.py
+++ appmanager/appmanager/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = 'appmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 #DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'django_db',
-#         'USER':  'django_user',
-#         'PASSWORD': 'pwd',
-#         'HOST':  'localhost',
-#         'PORT':  5432
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-# }
+#}
+
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'django_db',
+         'USER':  'django_user',
+         'PASSWORD': 'pwd',
+         'HOST':  'db',
+         'PORT':  5432
+    }
+ }
 
 
 
