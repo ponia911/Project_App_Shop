5643d633d39d75af2af25350d45c57762fd688f5
Andrei <poniaable@gmail.com>
2023-10-27 12:44:28 +0300

    mat

--- appmanager/cart/tests.py
+++ appmanager/cart/tests.py
@@ -1,3 +1,33 @@
+
 from django.test import TestCase
 
-# Create your tests here.
+from cart.cart import Cart
+from cart.views import cart_remove, cart_about
+
+
+
+
+
+
+class TestCart(TestCase):
+
+    def test_cart_add(self):
+        response = self.client.post('/cart/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_cart_remove(self):
+        request = self.client.post('/cart:cart_about/', {})
+        self.session = request.session
+        Cart(request).add(product=self.product, quantity=1)
+        response = cart_remove(request, product_id=self.product.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/cart:cart_about/')
+
+    def test_cart_about(self):
+        request = self.client.get('/cart:cart_about/')
+        self.session = request.session
+        Cart(request).add(product=self.product, quantity=1)
+        response = cart_about(request)
+        self.assertEqual(response.status_code, 200)
+
+
--- appmanager/shop/tests.py
+++ appmanager/shop/tests.py
@@ -1,6 +1,9 @@
+import unittest
+
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.test import TestCase
 from django.urls import reverse
+
 
 
 class TestShop(TestCase):
@@ -8,6 +11,7 @@ class TestShop(TestCase):
     def test_index(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
+
     def test_login(self):
         response = self.client.get('/login/')
         self.assertEquals(response.status_code, 200)
@@ -18,14 +22,7 @@ class TestRegisterView(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertIsInstance(response.context['form'], UserCreationForm)
 
-    def tets_post_ok(self):
-        response = self.client.post('/register/')
-        self.assertEquals(response.status_code, 302)
-
-
-
-
-
-
-
-
+#class TestProductView(TestCase):
+#    def test_product(self):
+#        request = self.client.get('/football')
+#        self.assertEquals(request.status_code, 200)
